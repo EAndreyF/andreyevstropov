8c7be44ff629b8ea77f2949a0e3d11e4d31b14c6
EAndreyF <EAndreyF@yandex.ru>
2014-03-19 01:35:19 +0400

    main page information, contact page

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-$(function() {
+$(function () {
     'use strict';
-    $.when($.getJSON('data/phases.json'), 
+    $.when($.getJSON('data/phases.json'),
            $.ajax({ url: 't/main.html'}),
-           $.getScript('js/handlebars.min.js')).done(function(dataJSON, dataTemplate) {
+           $.getScript('js/handlebars.min.js')).done(function (dataJSON, dataTemplate) {
         
         $('head').append(dataTemplate[0]);
         
-        var $phases = $('.header__phases');
-        var template   = Handlebars.compile($("#phases_template").html());
-        dataJSON[0].forEach(function(element) {
+        var $phases = $('.header__phases'),
+            template   = Handlebars.compile($("#phases_template").html());
+        dataJSON[0].forEach(function (element) {
             $phases.append(template(element));
         });
         $phases.slide();
-    }).fail(function() {
+    }).fail(function () {
         console.log(arguments);
     });
 
-    $.fn.slide = function() {
-        var $this = $(this);
-        var $child = $('.blockquote', this);
-        var count = $child.length;
+    $.fn.slide = function () {
+        var $this = $(this),
+            $child = $('.blockquote', this),
+            count = $child.length;
         
         function getRandom() {
             return $child.eq(Math.ceil(Math.random() * count) - 1);
         }
         
         getRandom().fadeIn('slow');
-        setInterval(function() {
-            $child.has(':visible').fadeOut('slow', function() { 
+        setInterval(function () {
+            $child.has(':visible').fadeOut('slow', function () {
                 var rnd = getRandom();
                 console.log(rnd);
                 rnd.fadeIn('slow');
             });
-        }, 10000);
-    }
+        }, 15000);
+    };
 });
